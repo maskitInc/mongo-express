02c0670e5631cfe8e29f122d3b47582aac55cb53
Maksim Kubyshkin <maskit.inc@gmail.com>
2017-05-10 03:39:15 +0300

    THIS IS FUCKING AWESOME

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ const bootstrapPath = resolveModulePath('bootstrap');
 module.exports = {
   entry: {
     index: './lib/scripts/index.js',
-    database: './lib/scripts/database.js',
+    database: './lib/scripts/news.js',
     collection: './lib/scripts/collection.js',
     document: './lib/scripts/document.js',
     gridfs: './lib/scripts/gridfs.js',
